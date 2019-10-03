8d24f74b73bb7b67bfe6bd5a1542d37585a105d2
benjaminsinturel <benjamin.sinturel@hec.edu>
2019-10-03 15:21:16 +0200

    UPDATE assessing

--- assessing.py
+++ assessing.py
@@ -6,11 +6,16 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.stats import ttest_ind
 
 
+
+
 ### Constants
-train_test_split = 0.8
 tag_column = 'tags'
+tokens_column = 'text'
+title_column = 'Title'
+train_test_split = 0.8
 pvalue_threshold  = 0.05
-tokens_col = 'text'
+
+
 
 
 
@@ -20,19 +25,27 @@ tokens_col = 'text'
 # Method 1: Robustness
 def split_train_test(df, train_test_split=0.8):
     '''
-    Create two dataframes: one with 100% of the tokens and other one with a percentage of the data
+    Randomly selects a percentage of tokens
+
+    :param df: the preprocessed dataframe
+    :param train_test_split: the percentage of tokens to randomly select
+    :return: two dataframes: one with 100% of the tokens  and one with a percentage of the tokens
     '''
     df_test = df.copy()
-    df_test[tokens_col] = df.apply(lambda x: random.sample(x[tokens_col],round(len(x[tokens_col]) * train_test_split)), axis=1)
+    df_test[tokens_column] = df.apply(lambda x: random.sample(x[tokens_column],round(len(x[tokens_column]) * train_test_split)), axis=1)
 
     return df, df_test
 
 
 # Method 2: Separation
-def get_seperation_evaluation(tag_column, unique_tags, pvalue_threshold=0.05):
+def evaluate_separation(tag_column, unique_tags, pvalue_threshold=0.05):
     '''
-    Returns if the mean of the tfidf of the cluster with a label is statistically different from the group which has not this label
-    (t-test)
+    Assess if the tag of a cluster is significantly more in the cluster than in the rest of the corpus
+
+    :param tag_column: tag column name
+    :param unique_tags: list of unique tags
+    :param pvalue_threshold: threshold to reject the null hypothesis
+    :return: binary value (1 if the mean of the tfidf of the one cluster in statistically different from the rest of the corpus)
     '''
 
     # Compute tfidf
@@ -55,14 +68,37 @@ def get_seperation_evaluation(tag_column, unique_tags, pvalue_threshold=0.05):
 
 # Method 3: Relevance
 
+def relevance_ratio(row):
+    '''
+    Computes the relevance ratio for a title
 
+    :param row: row of the preprocessed dataframe
+    :return: same row with a relevance metric
+    '''
+    count = 0
+    nb_words_title = len(row[title_column])
+
+    for tag in row[tag_column]:
+        if tag in row[title_column]:
+            count += 1
+
+    relevance_metric = count / nb_words_title
+    row["relevance_metric"] = relevance_metric
+
+    return row
+
+
+def evaluate_relevance(df):
+    df.apply(relevance_ratio, axis=1)
+    df.apply(pertinence_category)
 
 ### Execution flow
 #M1:
+
 df, df_test = split_train_test(df)
 
 #M2:
 #unique_tags = # todo: add Gautier function
-get_seperation_evaluation(tag_column, unique_tags)
+evaluate_separation(tag_column, unique_tags)
 
 
