a096a8457a0c1e94fc6b3ed4aa318cb9ad89fd88
GD <gautier.dulac@polytechnique.Edu>
2019-10-07 15:10:13 +0200

    Visualisation à date, non fonctionnelle

--- visualization.py
+++ visualization.py
@@ -16,26 +16,31 @@ def main_visualization(model_res, parameters):
     column_list = ["Node_name_" + str(i) for i in np.arange(1, max_depth + 1)] + \
                   ["Article_ID_List"] + \
                   ["Node_number_" + str(i) for i in np.arange(1, max_depth + 1)]
-    tableau_df = pd.DataFrame(columns=column_list)
+    tableau_list = []
+    tempo_node_names = [""] * max_depth
+    tempo_node_numbers = [""] * max_depth
     max_tags_tempo = [0] * max_depth
 
-    def fill_df_recurcive(local_node, tempo_node_names, tempo_node_numbers, depth):
-        global tableau_df
-        try:
+    def fill_df_recurcive(tableau_list, tempo_node_names, tempo_node_numbers, local_node, depth):
+        if len(local_node.children_clusters) > 0:
             # call function on children
+            print("Try:")
             children = local_node.children_clusters
             for child_number in range(len(children)):
                 max_tags_tempo[depth] += 1
-                fill_df_recurcive(children[child_number],
-                                  tempo_node_names.append(children[child_number].tag),
-                                  tempo_node_numbers.append(max_tags_tempo[depth]),
+                tempo_node_names[depth] = children[child_number].tag
+                tempo_node_numbers[depth] = max_tags_tempo[depth]
+                tableau_list = fill_df_recurcive(tableau_list, tempo_node_names, tempo_node_numbers,
+                                  children[child_number],
                                   depth + 1)
-        except:
+        else:
             # add leaf
-            information_list = tempo_node_names + [str(local_node.index_list())] + tempo_node_numbers
-            added_row = pd.Series(information_list, index=column_list)
-            tableau_df = pd.concat(tableau_df, added_row.to_frame().T)
-            return ()
+            print("Coucou")
+            information_list = [tempo_node_names + [str(local_node.index_list)] + tempo_node_numbers]
+            tableau_list = tableau_list.append(pd.DataFrame(information_list, columns=column_list))
+            print(tableau_list)
+            return tableau_list
 
-    fill_df_recurcive(model_res, [], [], 0)
-    return tableau_df
+    tableau_df_res = pd.DataFrame(fill_df_recurcive(tableau_list, tempo_node_names, tempo_node_numbers, model_res, 0),
+                                  columns=column_list)
+    return tableau_df_res
