b96bad7a18cdb23a304b49214a0b3ce57e8a6129
GD <gautier.dulac@polytechnique.Edu>
2019-10-10 00:33:46 +0200

    Visaulisation improvements

--- visualization.py
+++ visualization.py
@@ -1,6 +1,9 @@
 ### Imports
 import pandas as pd
 import numpy as np
+import csv
+import copy
+
 
 ### Main function
 def main_visualization(model_res, parameters):
@@ -21,26 +24,30 @@ def main_visualization(model_res, parameters):
     tempo_node_numbers = [""] * max_depth
     max_tags_tempo = [0] * max_depth
 
-    def fill_df_recurcive(tableau_list, tempo_node_names, tempo_node_numbers, local_node, depth):
+    def fill_df_recurcive(spamwriter, tempo_node_names, tempo_node_numbers, local_node, depth):
         if len(local_node.children_clusters) > 0:
             # call function on children
-            print("Try:")
             children = local_node.children_clusters
             for child_number in range(len(children)):
                 max_tags_tempo[depth] += 1
+                tempo_node_names = copy.copy(tempo_node_names)
                 tempo_node_names[depth] = children[child_number].tag
+                tempo_node_numbers = copy.copy(tempo_node_numbers)
                 tempo_node_numbers[depth] = max_tags_tempo[depth]
-                tableau_list = fill_df_recurcive(tableau_list, tempo_node_names, tempo_node_numbers,
+                fill_df_recurcive(spamwriter, tempo_node_names, tempo_node_numbers,
                                   children[child_number],
                                   depth + 1)
         else:
             # add leaf
-            print("Coucou")
-            information_list = [tempo_node_names + [str(local_node.index_list)] + tempo_node_numbers]
-            tableau_list = tableau_list.append(pd.DataFrame(information_list, columns=column_list))
-            print(tableau_list)
-            return tableau_list
+            information_list = tempo_node_names + [str(local_node.index_list)] + tempo_node_numbers
+            spamwriter.writerow(information_list)
+            return ()
 
-    tableau_df_res = pd.DataFrame(fill_df_recurcive(tableau_list, tempo_node_names, tempo_node_numbers, model_res, 0),
-                                  columns=column_list)
+    with open('tableau_res.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=';')
+        spamwriter.writerow(column_list)
+        fill_df_recurcive(spamwriter, tempo_node_names, tempo_node_numbers, model_res, 0)
+
+    tableau_df_res = pd.read_csv("tableau_res.csv")
+
     return tableau_df_res
